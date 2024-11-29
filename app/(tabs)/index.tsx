c1fcbe2211801d227ef18b1ed7341db9728438ea
Normand <normandlubaton@icloud.com>
2024-11-30 00:28:55 +0800

    Mock refresh, card component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,46 +1,28 @@
+import Details from "@/components/Home/Details";
 import * as React from "react";
-import { SafeAreaView, View } from "react-native";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Text } from "@/components/ui/text";
-import { ThemeToggle } from "@/components/ThemeToggle";
+import { RefreshControl, SafeAreaView, ScrollView, View } from "react-native";
 
 export default function Screen() {
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
   return (
     <SafeAreaView className="flex-1 items-center gap-5 p-6 bg-background">
-      <Card className="w-full max-w-sm p-6 rounded-2xl">
-        <CardHeader className="items-center">
-          <View className="p-3">
-            <CardTitle className="pb-2 text-center">Testing</CardTitle>
-          </View>
-          <View className="flex-row">
-            <CardDescription className="text-base font-semibold">
-              <ThemeToggle />
-            </CardDescription>
-          </View>
-        </CardHeader>
-        <CardContent>
-          <View className="flex-row justify-around gap-3">
-            <View className="items-center">
-              <Text className="text-sm text-muted-foreground">Div</Text>
-              <Text className="text-xl font-semibold">1</Text>
-            </View>
-            <View className="items-center">
-              <Text className="text-sm text-muted-foreground">Div</Text>
-              <Text className="text-xl font-semibold">2</Text>
-            </View>
-            <View className="items-center">
-              <Text className="text-sm text-muted-foreground">Div</Text>
-              <Text className="text-xl font-semibold">3</Text>
-            </View>
-          </View>
-        </CardContent>
-      </Card>
+      <ScrollView
+        className="w-full"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ flexGrow: 1, alignItems: "center" }}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        <Details />
+      </ScrollView>
     </SafeAreaView>
   );
 }
---
+++ components/Home/Details.tsx
@@ -0,0 +1,46 @@
+import { View } from "react-native";
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Text } from "@/components/ui/text";
+import { ThemeToggle } from "@/components/ThemeToggle";
+
+const Details = () => {
+  return (
+    <Card className="w-full max-w-sm p-6 rounded-2xl mt-8">
+      <CardHeader className="items-center">
+        <View className="p-3">
+          <CardTitle className="pb-2 text-center">Testing</CardTitle>
+        </View>
+        <View className="flex-row">
+          <CardDescription className="text-base font-semibold">
+            <ThemeToggle />
+          </CardDescription>
+        </View>
+      </CardHeader>
+      <CardContent>
+        <View className="flex-row justify-around gap-3">
+          <View className="items-center">
+            <Text className="text-sm text-muted-foreground">Div</Text>
+            <Text className="text-xl font-semibold">1</Text>
+          </View>
+          <View className="items-center">
+            <Text className="text-sm text-muted-foreground">Div</Text>
+            <Text className="text-xl font-semibold">2</Text>
+          </View>
+          <View className="items-center">
+            <Text className="text-sm text-muted-foreground">Div</Text>
+            <Text className="text-xl font-semibold">3</Text>
+          </View>
+        </View>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Details;
